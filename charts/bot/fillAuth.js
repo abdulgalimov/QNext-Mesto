1d6f6d2444780f98f000330f9654b46048755f48
Zaur Abdulgalimov <abdulgalimov@corp.mail.ru>
2021-10-06 11:42:34 +0000

    bot chart

--- charts/bot/fillAuth.js
+++ charts/bot/fillAuth.js
@@ -2,16 +2,23 @@ const {divider, where} = qnext.getValue('localVar');
 
 const targets = [];
 async function run() {
-  const {block} = await qnext.tasks.parallel({
+  const {block, unblock} = await qnext.tasks.parallel({
     block: qnext.customStats.read({
       select: 'count() as value, (intDiv(toUInt32(createdDate), '+divider+') * '+divider+') * 1000 as time',
       where: where.time + ' param1 = 4 ',
       group: 'time',
       order: 'time'
     }),
+    unblock: qnext.customStats.read({
+      select: 'count() as value, (intDiv(toUInt32(createdDate), '+divider+') * '+divider+') * 1000 as time',
+      where: where.time + ' param1 = 5 ',
+      group: 'time',
+      order: 'time'
+    }),
   });
   exports.block = block;
-  targets.push(getTarget('Блокировка', block));
+  targets.push(getTarget('🚫Блокировка', block));
+  targets.push(getTarget('✅Разблокировка', unblock));
   exports.targets = targets;
 }
 
