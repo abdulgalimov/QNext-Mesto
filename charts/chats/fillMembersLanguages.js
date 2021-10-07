c6b816e90abe8a6ab7cb5eae3bdbecb1a35d371f
Zaur Abdulgalimov <abdulgalimov@corp.mail.ru>
2021-10-07 15:14:53 +0000

    charts

--- charts/chats/fillMembersLanguages.js
+++ charts/chats/fillMembersLanguages.js
@@ -2,21 +2,32 @@ const request = qnext.getValue('request');
 const {divider, target, viewChat} = qnext.getValue('localVar');
 
 async function run() {
-  const whereChat = viewChat !== 'all' ? ' AND param2='+viewChat : '';
+  const whereChat = viewChat !== 'all' ? ' AND param2=' + viewChat : '';
   const {membersLanguages} = await qnext.tasks.parallel({
     membersLanguages: qnext.customStats.read({
-      select: 'now() AS time, param5 as code, count() AS cnt',
-      where: 'param1 = 2003 AND param5 > 0 '+whereChat,
-      group: 'param5',
+      select: 'now() AS time, param5 as code, count() AS cnt, param3 as userId',
+      where: 'param1 = 2003 AND param5 > 0 ' + whereChat,
+      group: 'param5,param3',
     })
   });
   exports.membersLanguages = membersLanguages;
-  exports.targets = membersLanguages.result.response.map(item => {
-    const language = qnext.isoLanguages.getById(item.code);
+  //
+  const byCodes = {};
+  membersLanguages.result.response.map(item => {
+    byCodes[item.code] = byCodes[item.code] || 0
+    byCodes[item.code] ++;
+  });
+  exports.byCodes = byCodes;
+  //
+  exports.targets = Object.keys(byCodes).map(key => {
+    const code = +key||0;
+    const count = byCodes[key];
+    const language = qnext.isoLanguages.getById(code);
+    const name = language ? language.name : qnext.isoLanguages.decode(code);
     return {
-      target: language ? language.name : 'Unknown',
+      target: name||'Unknown',
       datapoints: [[
-        item.cnt, 0,
+        count, 0,
       ]]
     }
   })
