a8100e2640169056de3ac68858bdd24592d3764f
Zaur Abdulgalimov <abdulgalimov@corp.mail.ru>
2022-02-15 19:08:12 +0000

    dev

--- scripts/statsSql.js
+++ scripts/statsSql.js
@@ -2,6 +2,10 @@
 let text = qnext.getValue('update.message.text', '');
 
 const reg = /^select\s+(?<who>[^;]+)\s+from\s+stats\s+where\s+(?<where>[^;]+);?$/ig;
+const ignoreKeys = {
+  botId: 1,
+  version: 1,
+};
 async function run() {
   if (!text) {
     exports.error = true;
@@ -19,6 +23,32 @@ async function run() {
   const sql = `SELECT ${who} FROM CustomStats WHERE botId=8802 and version=3 AND ${where}`;
   const result = await qnext.customStats.readSql(sql);
   console.log('result', sql, result);
+  if (result.ok) {
+    if (!result.response.length) {
+      exports.message = 'пусто';
+      return;
+    }
+    const spaces = [];
+    const keys = Object
+      .keys(result.response[0])
+      .filter(key => !ignoreKeys[key])
+      .map(key => {
+        spaces.push(key.length);
+        return key;
+      });
+    const lines = [keys];
+    let values = result.response;
+    if (values.length > 20) values = values.slice(0, 20);
+    values.map(item => {
+      const line = keys.map((key, index) => {
+        const value = item[key]
+        spaces[index] = Math.max(spaces[index], (''+value).length);
+        return value;
+      })
+      lines.push(line);
+    })
+    console.log('values', keys, spaces, lines);
+  }
 
 }
 
