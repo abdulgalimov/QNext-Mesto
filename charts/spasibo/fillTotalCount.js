d50420f6c97f7430f3b41c900fe8737ddff6715b
Zaur Abdulgalimov <abdulgalimov@corp.mail.ru>
2022-02-15 16:04:46 +0000

    dev

--- charts/spasibo/fillTotalCount.js
+++ charts/spasibo/fillTotalCount.js
@@ -4,10 +4,8 @@ const targets = [];
 async function run() {
   const {membersCount} = await qnext.tasks.parallel({
     membersCount: qnext.customStats.read({
-      select: 'max(param3) as value, (intDiv(toUInt32(createdDate), '+divider+') * '+divider+') * 1000 as time',
+      select: 'count() as value, createdDate as time',
       where: where.time+' param1 = 2009 ',
-      group: 'time',
-      order: 'time'
     })
   });
   targets.push(getTarget('Всего Spasibo', membersCount));
