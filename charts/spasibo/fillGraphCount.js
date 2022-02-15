3197a2ed7cea3f8a29571714142982239d728743
Zaur Abdulgalimov <abdulgalimov@corp.mail.ru>
2022-02-15 16:02:18 +0000

    dev

--- charts/spasibo/fillGraphCount.js
+++ charts/spasibo/fillGraphCount.js
@@ -2,7 +2,7 @@ const {divider, where} = qnext.getValue('localVar');
 
 const targets = [];
 async function run() {
-  const counts = qnext.customStats.read({
+  const counts = await qnext.customStats.read({
     select: 'count() as value, (intDiv(toUInt32(createdDate), '+divider+') * '+divider+') * 1000 as time',
     where: `${where.time} param1 = 2009 and param2 != -1001501275697 ${where.user}`,
     group: 'time',
