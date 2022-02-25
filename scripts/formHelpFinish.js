f32eeb93c6825f313a1f9b8a01b3fc24ab6acc94
Zaur Abdulgalimov <abdulgalimov@corp.mail.ru>
2022-02-25 15:18:53 +0000

    dev

---
+++ scripts/formHelpFinish.js
@@ -0,0 +1,48 @@
+
+const {user, formResult} = qnext.data;
+const {type, housing, psychological, documents, humanitarian, medical, transport, tg, contacts} = formResult.values;
+
+let message;
+switch (type) {
+  case 'Могу помочь':
+    buildCanHelp();
+    break;
+  case 'Нужна помощь':
+    buildNeedHelp();
+    break;
+}
+
+const {italic} = qnext.html;
+function addLine(key, value) {
+  if (!value) return;
+  message += `\n${italic(key)}: ${value}`;
+}
+function buildCanHelp() {
+  message = `💙 Могу помочь.`;
+  addLine('С жильем', housing);
+  addLine('Психологическая помощь', psychological);
+  addLine('Документы и юридическая помощь', documents);
+  addLine('Гуманитарная помощь', humanitarian);
+  addLine('Медицинская помощь', medical);
+  addLine('Транспортная помощь', transport);
+  addLine('Контакты', contacts);
+}
+
+function buildNeedHelp() {
+  message = `❤️ Нужна помощь.`;
+  addLine('С жильем', housing);
+  addLine('Психологическая помощь', psychological);
+  addLine('Документы и юридическая помощь', documents);
+  addLine('Гуманитарная помощь', humanitarian);
+  addLine('Медицинская помощь', medical);
+  addLine('Транспортная помощь', transport);
+  addLine('Контакты', contacts);
+}
+
+if (tg === 'Да') {
+  message = `${message}
+${qnext.urls.user(user)}`
+}
+
+
+exports.message = message;
