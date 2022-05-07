8152bb59d2a7346bcc45c99aa4c2636e126ea594
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2022-05-08 02:18:18 +0300

    spasibot stats

--- scripts/statsSpasiboView.js
+++ scripts/statsSpasiboView.js
@@ -6,14 +6,14 @@ const byId = users.reduce((data, user) => {
   return data;
 }, {});
 
+let num = 1;
 const places = userIds
-  .map((userId, index) => {
-    const num = index + 1;
+  .map(userId => {
     const user = byId[userId];
     if (!user) return;
     const count = counts[userId]||0;
     const userName = qnext.html.link(user.name, qnext.urls.user(user));
-    return `${num}) ${count} - ${userName}`;
+    return `${num++}) ${count} - ${userName}`;
   })
   .filter(line => !!line);
 exports.message = places.join('\n');
