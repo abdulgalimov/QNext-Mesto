110b82b1c1ac3916206be566f45f442fc9a5ad56
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2022-05-17 21:44:11 +0300

    dev

--- scripts/hr/resumeFinish.js
+++ scripts/hr/resumeFinish.js
@@ -3,5 +3,33 @@ const {
   name,
   contacts,
   position,
-  format
+  format,
+  about,
+  keys,
+  salary,
+  currency
 } = qnext.data.formResult.values;
+
+const {
+  resume_link,
+} = qnext.data.formResult.data;
+
+
+function parseSalary() {
+  const normalized = salary
+    .replaceAll(' ', '')
+    .replace(',', '.');
+  return normalized||0;
+}
+
+exports.doc = {
+  name,
+  contacts,
+  position,
+  format: format.join(',').map(value => value.split()),
+  about,
+  keys: keys.join(',').map(value => value.split()),
+  resumeLink: resume_link.text,
+  currency,
+  salary: parseSalary(),
+}
