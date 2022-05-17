204d04adaafefec573fa1e269892ac352f0eb08f
Zaur Abdulgalimov <abdulgalimov@gmail.com>
2022-05-17 22:02:33 +0300

    dev

---
+++ scripts/hr/resumeView.js
@@ -0,0 +1,26 @@
+
+const {dataValue} = qnext.data;
+
+const {
+  name,
+  contacts,
+  position,
+  format,
+  about,
+  keys,
+  resumeLink,
+  currency,
+  salary,
+} = dataValue.fields;
+
+let view = `Резюме ${position}
+${about}
+
+Зарплатные ожидания: ${salary}${currency}
+Ключевые навыки: ${keys}
+Формат работы: ${format}
+Контакты: ${contacts}`
+if (resumeLink) {
+  view = `${view}
+${qnext.html.link('Ссылка на резюме.', resumeLink)}`;
+}
